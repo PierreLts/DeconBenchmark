d76fc3d7c5ca8c77391ade4388a5acf4be2f85c6
PierreLts <pierre.lorthiois@epfl.ch>
2025-03-13 19:30:24 +0100

    adjust model list

--- scripts/deconvolution/parallel_deconv.sh
+++ scripts/deconvolution/parallel_deconv.sh
@@ -5,7 +5,8 @@
 DEFAULT_DATASET_PREFIX="TB1-random"
 DEFAULT_SAMPLE_FILTER="B"
 DEFAULT_BULK_TYPE="bulk_random"  # New parameter for bulk file type
-DEFAULT_METHODS="AdRoit,ARIC,AutoGeneS,BayCount,BayesPrism,BayICE,BisqueMarker,BisqueRef,BseqSC,CDSeq,CellDistinguisher,CIBERSORT,CIBERSORTx,CPM,DAISM,debCAM,Deblender,DeCompress,deconf,DeconICA,DeconPeaker,DeconRNASeq,deconvSeq,DecOT,DeMixT,DESeq2,digitalDLSorter,DSA,dtangle,DWLS,EMeth,EPIC,FARDEEP,ImmuCellAI,LinDeconSeq,Linseed,MCPcounter,MIXTURE,MOMF,MuSic,MySort,NITUMID,PREDE,quanTIseq,RNA-Sieve,scaden,SCDC,spatialDWLS,TOAST"
+DEFAULT_METHODS="ARIC,AutoGeneS,BayCount,BayesPrism,BayICE,BisqueRef,CDSeq,CPM,deconf,DeconICA,DeconPeaker,DeconRNASeq,deconvSeq,DESeq2,DSA,DWLS,EMeth,EPIC,FARDEEP,LinDeconSeq,Linseed,MCPcounter,MIXTURE,MuSic,MySort,PREDE,quanTIseq,RNA-Sieve,scaden,SCDC,TOAST"
+# "AdRoit,ARIC,AutoGeneS,BayCount,BayesPrism,BayICE,BisqueMarker,BisqueRef,BseqSC,CDSeq,CellDistinguisher,CIBERSORT,CIBERSORTx,CPM,DAISM,debCAM,Deblender,DeCompress,deconf,DeconICA,DeconPeaker,DeconRNASeq,deconvSeq,DecOT,DeMixT,DESeq2,digitalDLSorter,DSA,dtangle,DWLS,EMeth,EPIC,FARDEEP,ImmuCellAI,LinDeconSeq,Linseed,MCPcounter,MIXTURE,MOMF,MuSic,MySort,NITUMID,PREDE,quanTIseq,RNA-Sieve,scaden,SCDC,spatialDWLS,TOAST"
 
 # Parse command line arguments
 DATASET_PREFIX="${1:-$DEFAULT_DATASET_PREFIX}"
